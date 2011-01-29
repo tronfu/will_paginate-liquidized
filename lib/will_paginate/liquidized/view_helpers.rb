c820f04ba75dd456ae649de4b0f9848b21dc3092
tronfu <tron@riverwatcher.com>
2011-01-29 06:33:05 -0800

    Updated the view_helpers to be compatible with WillPaginate 3.0. Renamed the liquid method to will_paginate from will_paginate_liquid.

--- lib/will_paginate/liquidized/view_helpers.rb
+++ lib/will_paginate/liquidized/view_helpers.rb
@@ -1,8 +1,10 @@
 module WillPaginate::Liquidized   
   module ViewHelpers
-    include WillPaginate::ViewHelpers
+    include WillPaginate::ViewHelpers::ActionView
     
-    def will_paginate_liquid(collection, anchor = nil, prev_label = nil, next_label = nil)      
+    alias_method :will_paginate_original, :will_paginate
+    
+    def will_paginate(collection, anchor = nil, prev_label = nil, next_label = nil)      
       opts = {}
       opts[:previous_label] = prev_label if prev_label
       opts[:next_label]     = next_label if next_label      
@@ -10,7 +12,7 @@ module WillPaginate::Liquidized
       opts[:controller]     = @context.registers[:controller]
       
       with_renderer 'WillPaginate::Liquidized::LinkRenderer' do 
-        will_paginate *[collection, opts].compact
+        will_paginate_original *[collection, opts].compact
       end
     end
     
@@ -26,71 +28,57 @@ module WillPaginate::Liquidized
     end
   end
 
-  class LinkRenderer < WillPaginate::LinkRenderer
+  class LinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
     
     include ActionView::Helpers::UrlHelper
     include ActionView::Helpers::TagHelper 
-
-    def to_html
-      return "<p><strong style=\"color:red;\">(Will Paginate Liquidized) Error:</strong> you must pass a controller in Liquid render call; <br/>
-              e.g. Liquid::Template.parse(\"{{ movies | will_paginate_liquid }}\").render({'movies' => @movies}, :registers => {:controller => @controller})</p>" unless @options[:controller]
-      
-      links = @options[:page_links] ? windowed_links : []
-      # previous/next buttons added in to the links collection
-      links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
-      links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
-      
-      html = links.join(@options[:separator])
-      html_attributes.delete(:controller)
-      @options[:container] ? content_tag(:div, html, html_attributes) : html
+    
+    def tag(name, value, attributes = {})
+      string_attributes = attributes.inject('') do |attrs, pair|
+        unless pair.last.nil?
+          attrs << %( #{pair.first}="#{CGI::escapeHTML(pair.last.to_s)}")
+        end
+        attrs
+      end
+      "<#{name}#{string_attributes}>#{value}</#{name}>"
     end
     
-    def page_link(page, text, attributes = {})
-      link_to text, url_for_page(page), attributes
-    end
-
-    def page_span(page, text, attributes = {})
-      content_tag :span, text, attributes
-    end
-              
-    def url_for_page(page)
-      page_one = page == 1
-      unless @url_string and !page_one
-        @url_params = {}
-        @controller = @options[:controller]
-        # page links should preserve GET parameters
-        stringified_merge @url_params, @controller.params if @controller && @controller.request.get? && @controller.params
-        stringified_merge @url_params, @options[:params] if @options[:params]
-        
-        if complex = param_name.index(/[^\w-]/)
-          page_param = (defined?(CGIMethods) ? CGIMethods : ActionController::AbstractRequest).
-            parse_query_parameters("#{param_name}=#{page}")
-          
-          stringified_merge @url_params, page_param
-        else
-          @url_params[param_name] = page_one ? 1 : 2
-        end
-
-        url = @controller.url_for(@url_params)
-        url = "#{url}##{@options[:params][:anchor]}" if @options[:params] && @options[:params][:anchor]
-        return url if page_one
-        
-        if complex
-          @url_string = url.sub(%r!((?:\?|&amp;)#{CGI.escape param_name}=)#{page}!, '\1@')
-          return url
-        else
-          @url_string = url
-          @url_params[param_name] = 3
-          @controller.url_for(@url_params).split(//).each_with_index do |char, i|
-            if char == '3' and url[i, 1] == '2'
-              @url_string[i] = '@'
-              break
-            end
-          end
-        end
+    def link(text, target, attributes = {})
+      if target.is_a? Fixnum
+        attributes[:rel] = rel_value(target)
+        target = url(target)
       end
-      # finally!
-      @url_string.sub '@', page.to_s
+      attributes[:href] = target
+      tag(:a, text, attributes)
+    end
+    
+    alias_method :to_html_original, :to_html
+    
+    def url(page)
+      @base_url_params ||= begin
+        url_params = base_url_params
+        merge_optional_params(url_params)
+        url_params
+      end
+      
+      url_params = @base_url_params.dup
+      add_current_page_param(url_params, page)
+      
+      @options[:controller].url_for(url_params)
+    end
+  
+    def base_url_params
+      url_params = default_url_params
+      # page links should preserve GET parameters
+      symbolized_update(url_params, @options[:controller].params) if @options[:controller].request.get?
+      url_params
+    end
+    
+    def to_html
+      return "<p><strong style=\"color:red;\">(Will Paginate Liquidized) Error:</strong> you must pass a controller in Liquid render call; <br/>
+              e.g. Liquid::Template.parse(\"{{ movies | will_paginate }}\").render({'movies' => @movies}, :registers => {:controller => @controller})</p>" unless @options[:controller]
+      
+      to_html_original
     end
   end  
 end        
